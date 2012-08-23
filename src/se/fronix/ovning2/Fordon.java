7cc6824b3d0b3a14b4706b9d97f43f12a23ff1aa
Fronix <fronix@fronix.se>
2012-08-23 13:56:27 +0200

    Övning 2, exemplet i boken funkar inte. Dunno why =S

---
+++ src/se/fronix/ovning2/Fordon.java
@@ -0,0 +1,14 @@
+package se.fronix.ovning2;
+
+public class Fordon {
+
+	public void skriv()
+	{
+		System.out.println("Ett fordon");
+	}
+	
+}
+
+class Motorfordon extends Fordon {
+	
+}
---
+++ src/se/fronix/ovning2/Hus.java
@@ -0,0 +1,17 @@
+package se.fronix.ovning2;
+
+public class Hus {
+	double längd;
+	double bredd;
+	int antalVåningar;
+	int senasteRenovering;
+	
+	Hus() {}
+	
+	Hus(double l, double b, int v)
+	{
+		längd = l;
+		bredd = b;
+		antalVåningar = v;
+	}
+}
